6ce95a702cf7e32c3a22f1a730fcf9e218210487
hoorainfatimaaa <hoorainfatimaa2005@gmail.com>
2026-08-07 13:40:27 +0300

    debug database url

--- backend/config.py
+++ backend/config.py
@@ -3,16 +3,15 @@ from dotenv import load_dotenv
 from datetime import timedelta
 
 load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL EXISTS:", bool(DATABASE_URL))
+print("DATABASE_URL START:", DATABASE_URL[:30] if DATABASE_URL else "MISSING")
+print("DATABASE_URL HAS_NONE:", "None" in DATABASE_URL if DATABASE_URL else False)
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{os.getenv('DB_USER')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:"
-        f"{os.getenv('DB_PORT')}/"
-        f"{os.getenv('DB_NAME')}"
-    )
-
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
